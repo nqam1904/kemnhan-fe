7c5875298bcd48539ee7ec4f7c954f8f13303fa4
Nguyen Phuoc Thao <fatrabbit1203@gmail.com>
2021-02-04 17:43:16 +0700

    Change history.push to link

--- src/components/user/landingpage/ItemProductShow.js
+++ src/components/user/landingpage/ItemProductShow.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { API_URL } from '../../../config/setting';
-import './ItemProductShow.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { API_URL } from "../../../config/setting";
+import "./ItemProductShow.css";
 
-const ItemProductShow = props => {
-    function getProduct(id) {
-        props.getProduct(id)
-    }
-    // console.log(props.images)
-    return (
-        <div className="card_item" onClick={() => getProduct(props.id)} >
-            <img className="image" src={`${API_URL}/static/${props.images}`} alt={props.name} />
-            <h5 id="title1" aria-hidden="true">{props.name}</h5>
-            <div className="overlay">
-                <div className="description">{props.description}</div>
-                {props.isFeature === true ? <div className="button_detail">Nổi bật</div> : ''}
-                {props.isActive === true ? "" : <div className="button_detail">Hết hàng</div>}
-            </div>
+const ItemProductShow = (props) => {
+  function getProduct(id) {
+    props.getProduct(id);
+  }
+  // console.log(props.images)
+  return (
+    <Link to={props.propsURL}>
+      <div className="card_item">
+        <img
+          className="image"
+          src={`${API_URL}/static/${props.images}`}
+          alt={props.name}
+        />
+        <h5 id="title1" aria-hidden="true">
+          {props.name}
+        </h5>
+        <div className="overlay">
+          <div className="description">{props.description}</div>
+          {props.isFeature === true ? (
+            <div className="button_detail">Nổi bật</div>
+          ) : (
+            ""
+          )}
+          {props.isActive === true ? (
+            ""
+          ) : (
+            <div className="button_detail">Hết hàng</div>
+          )}
         </div>
-    );
-}
+      </div>
+    </Link>
+  );
+};
 
 export default ItemProductShow;
