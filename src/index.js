41cfd854ea33988360cae1ddea33b42d8b817832
Minh <nghiemminh1904@gmail.com>
2021-02-05 21:11:33 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
 import configureStore, { history } from "./configureStore";
 import App from "./App";
-
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-activity/dist/react-activity.css";
 // const sagaMiddleware = createSagaMiddleware();
 let store = configureStore();
 
